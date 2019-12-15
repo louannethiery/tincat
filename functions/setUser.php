1028f0f46f682e1083e93a5aba26b92ad5641c76
Lou-Anne Thiery <louannethiery@gmail.com>
2019-12-15 18:32:35 +0100

    add input confirm password

--- functions/setUser.php
+++ functions/setUser.php
@@ -16,16 +16,20 @@ try {
 
 // Avant d'insérer en base de données faire les vérifications suivantes
     // Vérifier si le pseudo ou le mot de passe est vide
-    if ( empty( $pseudo ) || empty( $password ) || empty( $email )) {
+    if ( empty( $pseudo ) || empty( $password )) {
         echo "Merci de remplir le champ du formulaire vide";
     }
     // Ajouter un input confirm password et vérifier si les deux sont égaux
+    if ($_POST['password'] != $_POST['password2']) {
+        echo 'Les mots de passe doivent être identiques';
+    } 
 
 // Ajouter un champ email
 
 // Etape 3: prepare request
-$req = $db->prepare("INSERT INTO users (pseudo, password, email) VALUES(:pseudo, :password, :email)");
-$req->bindParam(":pseudo", $_POST["pseudo"]);
-$req->bindParam(":password", $_POST["password"]);
+$req = $db->prepare("INSERT INTO users (pseudo, email, password, password2) VALUES(:pseudo, :email, :password, :password2)");
+$req->bindParam(":pseudo", $_POST["pseudo"]);;
 $req->bindParam(":email", $_POST["email"]);
+$req->bindParam(":password", $_POST["password"]);
+$req->bindParam(":password2", $_POST["password2"]);
 $req->execute();
--- register.php
+++ register.php
@@ -5,8 +5,9 @@
         <h1>TINCAT</h1>
         <form action="functions/setUser.php" method="post">
             <input type="text" placeholder="pseudo" name="pseudo">
-            <input type="password" placeholder="password" name="password">
             <input type="email" placeholder="email" name="email" required><br>
+            <input type="password" placeholder="password" name="password">
+            <input type="password" placeholder="confirm password" name="password2">
             <input type="submit" value="register">
             
         </form>
